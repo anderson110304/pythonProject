90c4da3bac8b9fde117017a6c878d722efcff8d4
Diyas <diyasimashev211@gmail.com>
2023-10-25 15:07:30 +0600

    Lab2

--- win_1_21_lab/book/models.py
+++ win_1_21_lab/book/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Book(models.Model):
+    GENRE = (
+        ('Роман', 'Роман'),
+        ('Детектив', 'Детектив'),
+        ('Фэнтези', 'Фэнтези'),
+        ('Научная фантастика', 'Научная фантастика'),
+        ('Документальная литература', 'Документальная литература'),
+    )
+    genre = models.CharField(max_length=100, choices=GENRE, default=GENRE[0], null=True)
     title = models.CharField(max_length=30)
     description = models.TextField()
     image = models.ImageField(upload_to='')
--- win_1_21_lab/book/urls.py
+++ win_1_21_lab/book/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.book_view),
+    path('book_detail/<int:id>/', views.book_detail_view),
 ]
