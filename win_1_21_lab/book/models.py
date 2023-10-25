a6b82f741033ffc679260f588b4d182434d755ce
Diyas <diyasimashev211@gmail.com>
2023-10-25 10:27:09 +0600

    Lab1

--- win_1_21_lab/book/models.py
+++ win_1_21_lab/book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField(upload_to='')
+    cost = models.IntegerField()
+    created_date_book = models.DateField(null=True)
+
+    def __str__(self):
+        return self.title
--- win_1_21_lab/book/views.py
+++ win_1_21_lab/book/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def book_view(request):
+    book_value = models.Book.objects.all()
+    return render(request, 'book.html', {'book_key': book_value})
