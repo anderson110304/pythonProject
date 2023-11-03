192bc6fd929770c5fd735191b5dea398f860bae9
Diyas <diyasimashev211@gmail.com>
2023-11-04 03:55:15 +0600

    Lab4

--- win_1_21_lab/book/urls.py
+++ win_1_21_lab/book/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.book_view),
     path('book_detail/<int:id>/', views.book_detail_view),
+    path('book_list/', views.delete_book_post_view),
+    path('book_list/<int:id>/delete/', views.drop_book_view),
+    path('create_post_book/', views.create_book_post_view),
     path('add-comment/', views.create_book_view),
 ]
--- win_1_21_lab/book/views.py
+++ win_1_21_lab/book/views.py
@@ -23,3 +23,26 @@ def create_book_view(request):
         form = forms.ReviewForm()
 
     return render(request, 'create_review.html', {'form': form})
+
+
+def create_book_post_view(request):
+    method = request.method
+    if method == "POST":
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = forms.BookForm()
+    return render(request, 'create_book.html', {'form': form})
+
+
+def delete_book_post_view(request):
+    book_value = models.Book.objects.all()
+    return render(request, 'book_list.html', {'book_key': book_value})
+
+
+def drop_book_view(request, id):
+    book_id = get_object_or_404(models.Book, id=id)
+    book_id.delete()
+    return redirect('/')
